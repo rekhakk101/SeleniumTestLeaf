615714fd7aeee245a78789c502e7a18ab750932c
rekhakk101 <95239952+rekhakk101@users.noreply.github.com>
2021-12-27 14:20:21 +0530

    Update MissingElementInAnArray.java

--- src/main/java/assignments/week1/day2/MissingElementInAnArray.java
+++ src/main/java/assignments/week1/day2/MissingElementInAnArray.java
@@ -3,7 +3,7 @@ package assignments.week1.day2;
 public class MissingElementInAnArray {
 
 	public static void main(String[] args) {
-		int[] arr = {1,2,3,4,7,6,8};
+		int[] arr = {1,2,3,4,6,7,8};
 		
 		for(int i=arr[0]; i<arr.length; i++) {
 			if(i != arr[i-1]) {
